99a56fc51d77167c76483d2f9894e929550e60ae
Veselin Totev <v.totev@clabteam.com>
2016-02-08 12:17:52 +0200

    Admin

--- admin/language/bulgarian/analytics/google_analytics.php
+++ admin/language/bulgarian/analytics/google_analytics.php
@@ -7,18 +7,18 @@
  *      PayPal: veskoto@gmail.com
  *      Bitcoin: 1QG2f8mGtbYiHc6k8ZzV4WhfcmVHs79QiK
  */
-$_['heading_title']    = 'Google Analytics';
+$_['heading_title']    = 'Google статистика';
 
 // Text
-$_['text_analytics']   = 'Analytics';
-$_['text_success']	   = 'Success: You have modified Google Analytics!';
-$_['text_edit']        = 'Edit Google Analytics';
+$_['text_analytics']   = 'Статистика';
+$_['text_success']	   = 'Успех: Променихте Google статистика!';
+$_['text_edit']        = 'Редакция на Google статистика';
 $_['text_signup']      = 'Login to your <a href="http://www.google.com/analytics/" target="_blank"><u>Google Analytics</u></a> account and after creating your website profile copy and paste the analytics code into this field.';
 
 // Entry
-$_['entry_code']       = 'Google Analytics Code';
-$_['entry_status']     = 'Status';
+$_['entry_code']       = 'Код за Google статистика';
+$_['entry_status']     = 'Статус';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify Google Analytics!';
+$_['error_permission'] = 'Внимание: You do not have permission to modify Google Analytics!';
 $_['error_code']	   = 'Code required!';
