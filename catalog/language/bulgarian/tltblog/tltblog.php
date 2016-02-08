c514edb6500b18daaefff696429cc95d94341c31
Veselin Totev <v.totev@clabteam.com>
2016-02-08 12:12:50 +0200

    Admin

--- catalog/language/bulgarian/tltblog/tltblog.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Text
-$_['text_path'] = 'Blogs';
-$_['text_error'] = 'Page Not Found!';
-$_['text_related'] = 'Свързани продукти';
-$_['text_tags'] = '<i class="fa fa-tags"></i>';
-$_['text_tax'] = 'Ex Tax:';
--- catalog/language/bulgarian/tltblog/tlttag.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Text
-$_['text_title'] = 'Information';
-$_['text_path'] = 'Information';
-$_['text_error'] = 'Information tag not found!';
-
