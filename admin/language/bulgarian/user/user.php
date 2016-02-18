32d120e6aa0b53626adc693d0e456afd1d5903bf
Veselin Totev <v.totev@clabteam.com>
2016-02-18 12:29:00 +0200

    Catalog /admin / users

--- admin/language/bulgarian/user/user.php
+++ admin/language/bulgarian/user/user.php
@@ -1,22 +1,30 @@
 <?php
+/*
+ * OpenCart Bulgarian translation
+ * Author: Veselin Totev (veselin.totev@gmail.com)
+ * License: Free to use
+ * Donate:
+ *      PayPal: veskoto@gmail.com
+ *      Bitcoin: 1QG2f8mGtbYiHc6k8ZzV4WhfcmVHs79QiK
+ */
 // Heading
-$_['heading_title']     = 'Users';
+$_['heading_title']     = 'Потребители';
 
 // Text
-$_['text_success']      = 'Успешно: Променихте users!';
-$_['text_list']         = 'User Списък';
-$_['text_add']          = 'Добавяне User';
-$_['text_edit']         = 'Редакция User';
+$_['text_success']      = 'Успешно: Променихте потребители!';
+$_['text_list']         = 'Списък';
+$_['text_add']          = 'Добавяне';
+$_['text_edit']         = 'Редакция';
 
 // Column
-$_['column_username']   = 'Username';
+$_['column_username']   = 'Потребителско име';
 $_['column_status']     = 'Статус';
 $_['column_date_added'] = 'Датата на добавяне';
 $_['column_action']     = 'Действие';
 
 // Entry
-$_['entry_username']   	= 'Username';
-$_['entry_user_group'] 	= 'User Group';
+$_['entry_username']   	= 'Потребителско име';
+$_['entry_user_group'] 	= 'Група';
 $_['entry_password']   	= 'Парола';
 $_['entry_confirm']    	= 'Порвърди';
 $_['entry_firstname']  	= 'Име';
@@ -26,11 +34,11 @@ $_['entry_image']      	= 'Изображение';
 $_['entry_status']     	= 'Статус';
 
 // Error
-$_['error_permission'] 	= 'Внимание: Нямате права за редакция на users!';
-$_['error_account']    	= 'Внимание: You can not delete your own account!';
-$_['error_exists']     	= 'Внимание: Username is already in use!';
-$_['error_username']   	= 'Username must be between 3 and 20 characters!';
-$_['error_password']   	= 'Password трябва да е между 4 и 20 символа!';
-$_['error_confirm']    	= 'Password and password confirmation do not match!';
-$_['error_firstname']  	= 'First Name трябва да е между 1 и 32 символа!';
-$_['error_lastname']   	= 'Last Name трябва да е между 1 и 32 символа!';
+$_['error_permission'] 	= 'Внимание: Нямате права за редакция на потребители!';
+$_['error_account']    	= 'Внимание: Не можете да изтриете собсвения си акаунт!';
+$_['error_exists']     	= 'Внимание: Потребителското име съществува!';
+$_['error_username']   	= 'Потребителското име трябва да е между 3 и 20 символа!';
+$_['error_password']   	= 'Паролата трябва да е между 4 и 20 символа!';
+$_['error_confirm']    	= 'Паролите не съвпадат!';
+$_['error_firstname']  	= 'Името трябва да е между 1 и 32 символа!';
+$_['error_lastname']   	= 'Фамилията трябва да е между 1 и 32 символа!';
--- admin/language/bulgarian/user/user_group.php
+++ admin/language/bulgarian/user/user_group.php
@@ -1,23 +1,31 @@
 <?php
+/*
+ * OpenCart Bulgarian translation
+ * Author: Veselin Totev (veselin.totev@gmail.com)
+ * License: Free to use
+ * Donate:
+ *      PayPal: veskoto@gmail.com
+ *      Bitcoin: 1QG2f8mGtbYiHc6k8ZzV4WhfcmVHs79QiK
+ */
 // Heading
-$_['heading_title']     = 'User Groups';
+$_['heading_title']     = 'Групи потребители';
 
 // Text
-$_['text_success']      = 'Успешно: Променихте user groups!';
-$_['text_list']         = 'User Group';
-$_['text_add']          = 'Добавяне User Group';
-$_['text_edit']         = 'Редакция User Group';
+$_['text_success']      = 'Успешно: Променихте групи потребители!';
+$_['text_list']         = 'Групи потребители';
+$_['text_add']          = 'Добавяне';
+$_['text_edit']         = 'Редакция';
 
 // Column
-$_['column_name']       = 'User Group Name';
+$_['column_name']       = 'Име';
 $_['column_action']     = 'Действие';
 
 // Entry
-$_['entry_name']        = 'User Group Name';
-$_['entry_access']      = 'Access Permission';
-$_['entry_modify']      = 'Modify Permission';
+$_['entry_name']        = 'Име';
+$_['entry_access']      = 'Права за достъп';
+$_['entry_modify']      = 'Права за редакция';
 
 // Error
-$_['error_permission']  = 'Внимание: Нямате права за редакция на user groups!';
-$_['error_name']        = 'User Group Name трябва да е между 3 и 64 символа!';
-$_['error_user']        = 'Внимание: This user group cannot be deleted as it is currently assigned to %s users!';
+$_['error_permission']  = 'Внимание: Нямате права за редакция на групи потребители!';
+$_['error_name']        = 'Името трябва да е между 3 и 64 символа!';
+$_['error_user']        = 'Внимание: Тази група не може да се изтрие, защото се ползва от %s потребителя!';
